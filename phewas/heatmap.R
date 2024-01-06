9d7a2ad4785a717de3d7a1c5445323d5541568d4
KinbackSicke <40781055+KinbackSicke@users.noreply.github.com>
2024-01-06 12:01:02 +0800

    Phewas for BAG

--- phewas/heatmap.R
+++ phewas/heatmap.R
@@ -5,15 +5,31 @@ library(dplyr)
 library(reshape2)
 library(pheatmap)
 
-pheno <- 'brain'
+pheno <- 'blood'
+bag_type <- 'bag'
 
-data_path <- sprintf('results_merged/heatmap_%s.csv', pheno)
+data_path <- sprintf('results_merged/heatmap_%s_%s.csv', pheno, bag_type)
 
-annot_path <- sprintf('results_merged/heatmap_%s_annot.csv', pheno)
+annot_path <- sprintf('results_merged/heatmap_%s_%s_annot.csv', pheno, bag_type)
 
 df <- read.csv(data_path, row.names = 'pheno')
 df_annot <- read.csv(annot_path, row.names = 'pheno')
 df_annot[is.na(df_annot)] <- ''
+
+# pheno <- 'brain'
+# bag_type <- 'bag'
+# 
+# data_path <- sprintf('results_merged/heatmap_%s_%s.csv', pheno, bag_type)
+# 
+# annot_path <- sprintf('results_merged/heatmap_%s_%s_annot.csv', pheno, bag_type)
+# 
+# df2 <- read.csv(data_path, row.names = 'pheno')
+# df_annot2 <- read.csv(annot_path, row.names = 'pheno')
+# df_annot2[is.na(df_annot2)] <- ''
+# 
+# df <- cbind(df, df2)
+# df_annot <- cbind(df_annot, df_annot2)
+
 
 if (pheno == 'blood') {
   show_row <- F
@@ -21,7 +37,7 @@ if (pheno == 'blood') {
   show_row <- T
 }
 
-res_path <- sprintf('heatmap/heat_%s_2.jpg', pheno)
+res_path <- sprintf('heatmap/heat_%s_%s.jpg', pheno, bag_type)
 
 pheatmap(df,
          annotation_row = NULL,
@@ -34,7 +50,7 @@ pheatmap(df,
          show_rownames = show_row, 
          show_colnames = T,
          cluster_rows = F,
-         cluster_cols = F,
+         cluster_cols = T,
          angle_col = 90,
          fontsize_number = 15,
          border_color = "#EEEEEE",
@@ -51,4 +67,5 @@ pheatmap(df,
          cellwidth = 15,
          cellheight = 15,
          # height = 15,
+         # width = 18,
          filename = res_path)
